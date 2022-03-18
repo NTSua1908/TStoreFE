93f3125e0376129923b293387e308474dd7a5945
LongThanh <19522232@gm.uit.edu.vn>
2022-03-18 15:52:16 +0700

    add deatil page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './pages/Home';
-
+import Game_detail from './pages/Game_detail';
 function App() {
   return (
     <div className="App">
-      <Header isLogin/>
+      <Header/>
       <Routes>
         <Route  path='/' element={<Home />}/>
+        <Route  path='/detail' element={<Game_detail />}/>
         <Route  path='*' element={<h1>Khong tim thay</h1>}/>
       </Routes>
     </div>
---
+++ src/pages/Game_detail.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/header/Header';
+
+function Game_detail(props){
+    return (
+        <div className="game-detail">
+            
+
+        </div>
+    )
+}
+
+export default Game_detail;
